67025a8803acb75b853ac09fcd63096d47c18151
wilsonmartinez1007 <wilson.martinez.rivera@correounivalle.edu.co>
2025-11-02 23:25:33 -0500

    Historia 4_Criterio1masCampos

--- app/src/main/java/com/univalle/inventory/view/MainActivity.kt
+++ app/src/main/java/com/univalle/inventory/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.univalle.inventory
+package com.univalle.inventory.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.univalle.inventory.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
