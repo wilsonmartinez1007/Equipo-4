8a42ab6a098b642569d6f253a79f74458e5f8b60
santi <Sant1RC>
2025-11-06 18:31:47 -0500

    feat(HU-6): Editar producto

--- app/src/main/java/com/univalle/inventory/data/InventoryDao.kt
+++ app/src/main/java/com/univalle/inventory/data/InventoryDao.kt
@@ -2,8 +2,11 @@ package com.univalle.inventory.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.Query
 import androidx.room.OnConflictStrategy
 import com.univalle.inventory.model.Inventory
+import androidx.lifecycle.LiveData
 
 @Dao
 interface InventoryDao {
@@ -13,4 +16,17 @@ interface InventoryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveInventory(inventory: Inventory)
 
+    // Funcion de actualizacr en la base de datos
+    @Update
+    suspend fun update(inventory: Inventory)
+
+    // Funcion para obtener un registro por id
+    @Query("SELECT * FROM inventory_table WHERE id = :itemId")
+    suspend fun getInventoryById(itemId: Int): Inventory?
+
+    // FUNCION TEMPORAL Cuenta la cantidad de registros en la DB
+    // Esta valida si hay registros previos para evitar duplicidad
+    // de los registros de prueba que se insertan en la db
+    @Query("SELECT COUNT(*) FROM inventory_table")
+    suspend fun getCount(): Int
 }
--- app/src/main/java/com/univalle/inventory/view/MainActivity.kt
+++ app/src/main/java/com/univalle/inventory/view/MainActivity.kt
@@ -6,10 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.univalle.inventory.R
+import com.univalle.inventory.data.InventoryDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import com.univalle.inventory.model.Inventory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // INSERTAR DATOS DE PRUEBA
+        insertarDatosPrueba()
+    }
+    private fun insertarDatosPrueba() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val dao = InventoryDB.getDatabase(this@MainActivity).inventoryDao()
+
+            // Insertar productos
+            if (dao.getCount() == 0) {
+                dao.saveInventory(Inventory(name = "Laptop Dell", price = 1500000, quantity = 5))
+                dao.saveInventory(Inventory(name = "Mouse Logitech", price = 45000, quantity = 20))
+                dao.saveInventory(Inventory(name = "Teclado Mecánico", price = 180000, quantity = 15))
+                dao.saveInventory(Inventory(name = "Monitor Samsung 24", price = 650000, quantity = 8))
+                dao.saveInventory(Inventory(name = "Webcam HD", price = 120000, quantity = 12))
+            }
+        }
+
     }
 }
